7ba2a92c754b2256ef24849ae770db3b221e11f1
priyapriyam <priyam18@navgurukul.org>
2019-05-31 11:04:23 +0530

    Fix.List inti list.

--- swap_positions.py
+++ swap_positions.py
@@ -3,7 +3,7 @@ def swapPositions(list, a, b):
     list[a], list[b] = list[b], list[a] 
     return list
    
-List = [2,4,6,10,15,8] 
+list = [2,4,6,10,15,8] 
 a, b  = 1,5
   
-print(swapPositions(List, a-1, b-1)) 
+print(swapPositions(list, a-1, b-1)) 
